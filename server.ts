1d031aa5ec45f2cdd3c91116b95abe309a73d79d
read-docs <u69_tymmos_lbn@technischools.com>
2026-01-09 15:07:51 +0100

    fix: vercel err

--- server.ts
+++ server.ts
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import type { Request, Response } from "express";
-import type { CryptoDetailDto } from "./contracts/cryptoDetail.dto";
-import type { CryptoBasicDto } from "./contracts/cryptoBasic.dto";
 
 const app = express();
 const port = 8080;
